d6c8b1e78da20e1035f32d8b0ceb4c4ae788193c
Romina-Zaino <rominazaino22@gmail.com>
2025-02-24 18:17:33 +0100

    fine esercitazione

---
+++ main.js
@@ -0,0 +1,17 @@
+
+//ESERCIZIO DEL 10.01.2025
+
+let num1 = 2
+let num2 = 4
+console.log("Il valore della variabile num1 è", num1)
+console.log("Il valore della variabile num2 è", num2)
+let stringa = "Sto recuperando"
+console.log("Il valore della variabile stringa è", stringa)
+
+
+stringa = "vorrei tanto recuperare"
+console.log (stringa)
+const PIGRECO =  3.14159265359
+console.log ("il valore di PIGRECO è", PIGRECO)
+PIGRECO = 0
+// non è possibile modificare una costante 
