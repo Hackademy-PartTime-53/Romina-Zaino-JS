1d9081357ea1f26d2480c8a3672e98a3e86eab58
Romina-Zaino <rominazaino22@gmail.com>
2025-02-24 19:00:50 +0100

    fine esercizio

---
+++ esercizioDel14Gennaio/main.js
@@ -0,0 +1,34 @@
+// esercizio del 14 gennaio
+let num1 = 2
+let num2 = 4
+let num3 = 6
+let num4 = 8
+let num5 = 10
+
+let somma = num1 + num2 + num3 + num4 + num5
+console.log(somma)
+let media = somma / 5
+console.log (media)
+
+console.log("La somma tra i numeri equivale a ", somma , "e la media equivale a", media)
+
+
+
+// esercizio 2
+let annoCorrente = 2025
+let annoDiNascita = 1997
+console.log (annoCorrente - annoDiNascita)
+let anni = annoCorrente - annoDiNascita
+let differenza = 100 - anni
+console.log("hai", anni, "anni e ti mancano", differenza, "anni per compierne 100" )
+
+
+// traccia 3
+let variabile1 = Number(prompt("inserisci qui un numero"));
+let vaariabile2 = Number( prompt("inserisci qui il numero"));
+let somma1 = variabile1 + vaariabile2
+let sottrazione = variabile1 - vaariabile2
+let moltiplicazione = variabile1 * vaariabile2
+let divisione = variabile1 / vaariabile2
+let potenza = variabile1 ** vaariabile2
+console.log("Con i numeri da te scelti, i risultati delle varie operazioni sono:somma",somma1, "sottrazione", sottrazione, "moltiplicazione" ,moltiplicazione, "divisione", divisione, "potenza", potenza)
